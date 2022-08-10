46228b5640e2007b3893323ad23160d9288b4f77
egluna-dev <egluna@live.ca>
2022-08-10 21:34:53 +1000

    Added square and exponential functions

--- main.py
+++ main.py
@@ -16,11 +16,20 @@ def multiply(n1, n2):
 def divide(n1, n2):
   return n1 / n2
 
+def square(n1):
+  return n1**2
+
+# Exponential function
+def exponent(n1, n2):
+  return n1**n2
+
 operations_dictionary = {
   "+": add,
   "-": subtract,
   "*": multiply,
   "/": divide,
+  "^": square,
+  "**": exponent,
 }
 
 def calculator():
@@ -33,20 +42,29 @@ def calculator():
   program_on = True
   
   while program_on:
+
     operation_symbol = input("Please pick an operation from the line above:\n")
-    operation_selected = operations_dictionary[operation_symbol]
-    num2 = float(input("What is the next number?\n"))
-    answer = operation_selected(num1, num2)
-  
-    print(f"{num1} {operation_symbol} {num2} = {answer}\n")
-  
-    continue_program = input("Please type 'y' to continue calculation or 'n' to start a new calculation.\n")
+    if operation_symbol == "^":
+      operation_selected = operations_dictionary[operation_symbol]
+      answer = operation_selected(num1)
+      print(f"{num1} ** 2 = {answer}")
+    else:
+
+      operation_selected = operations_dictionary[operation_symbol]
+      num2 = float(input("What is the next number?\n"))
+      answer = operation_selected(num1, num2)
+    
+      print(f"{num1} {operation_symbol} {num2} = {answer}\n")
+    
+    continue_program = input("Please type 'y' to continue calculation or 'n' to start a new calculation. Otherwise, please type 'end' to exit the program.\n")
   
     if continue_program == "y":
       num1 = answer
     elif continue_program == "n":
       program_on = False
       calculator()
+    elif continue_program == "end":
+      program_on = False
 
 calculator()
     
